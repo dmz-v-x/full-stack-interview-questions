50c186784bff4de47ee51b161c9b6d6fe47a8597
Himanshu Bhatt <74483969+dmz-v-x@users.noreply.github.com>
2026-03-18 16:31:12 +0530

    Create 004-square.js

---
+++ 009-interview-handbook/022/004-square.js
@@ -0,0 +1,2 @@
+Given a list of points, find out if any four of them form a square. Return 'true' if possible, else 'false'.
+Examples: [[0, 0], [2, 0], [1, 1], [0, -1], [-1, -1], [0, 2], [0, 1], [1,0]] -> true
